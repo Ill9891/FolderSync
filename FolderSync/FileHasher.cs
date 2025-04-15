e2fe8b1d93179558a30a014d445cc1ddb75fcc5b
Anna <hannalova1991@gmail.com>
2025-04-15 22:06:30 +0100

    Created FileHasher

---
+++ FolderSync/FileHasher.cs
@@ -0,0 +1,23 @@
+﻿using System.Security.Cryptography;
+
+namespace FolderSync
+{
+    public class FileHasher
+    {
+        private readonly Dictionary<string, string> _hashCache = new();
+
+        public string GetHash(string path)
+        {
+            if (_hashCache.TryGetValue(path, out var cached))
+                return cached;
+
+            using var md5 = MD5.Create();
+            using var stream = File.OpenRead(path);
+            var hash = md5.ComputeHash(stream);
+            var result = BitConverter.ToString(hash).Replace("-", "").ToLowerInvariant();
+
+            _hashCache[path] = result;
+            return result;
+        }
+    }
+}
