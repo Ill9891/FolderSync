144c3397eaa90159e225b15996b80628c456d567
Anna <hannalova1991@gmail.com>
2025-04-17 17:08:39 +0100

    Added unit test

---
+++ FolderSyncTests/FolderSynchronizerTests.cs
@@ -0,0 +1,31 @@
+﻿using FolderSync;
+using NUnit.Framework;
+
+namespace FolderSyncTests
+{
+    [TestFixture]
+    public class FolderSynchronizerTests : Hooks
+    {
+        [Test]
+        public void Synchronize_CopiesFilesFromSourceToReplica()
+        {
+            // Arrange
+            var fileName = "test.txt";
+            var filePath = Path.Combine(_sourceDir, fileName);
+            var fileText = "Hello, world!";
+            File.WriteAllText(filePath, fileText);
+
+            var synchronizer = new FolderSynchronizer(_sourceDir, _replicaDir, _logger);
+
+            // Act
+            synchronizer.Synchronize();
+
+            var replicaFilePath = Path.Combine(_replicaDir, fileName);
+
+            // Assert
+            Assert.That(File.Exists(replicaFilePath), Is.True, $"File '{replicaFilePath}' doesn't exist.");
+            var replicaContent = File.ReadAllText(replicaFilePath);
+            Assert.That(replicaContent, Is.EqualTo(fileText), $"Incorrect content of the file '{replicaFilePath}'.");
+        }
+    }
+}
---
+++ FolderSyncTests/Hooks.cs
@@ -0,0 +1,35 @@
+﻿using FolderSync;
+using NUnit.Framework;
+using System;
+namespace FolderSyncTests
+{
+    public class Hooks
+    {
+        protected string? _sourceDir;
+        protected string? _replicaDir;
+        protected string? _logDir;
+        protected Logger? _logger;
+
+        [SetUp]
+        public void Setup()
+        {
+            _sourceDir = Path.Combine(Path.GetTempPath(), Path.GetRandomFileName());
+            _replicaDir = Path.Combine(Path.GetTempPath(), Path.GetRandomFileName());
+            _logDir = Path.Combine(Path.GetTempPath(), Path.GetRandomFileName());
+
+            Directory.CreateDirectory(_sourceDir);
+            Directory.CreateDirectory(_replicaDir);
+            Directory.CreateDirectory(_logDir);
+
+            _logger = new Logger(Path.Combine(_logDir, "TestLogs"));
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            if (Directory.Exists(_sourceDir)) Directory.Delete(_sourceDir, true);
+            if (Directory.Exists(_replicaDir)) Directory.Delete(_replicaDir, true);
+            if (Directory.Exists(_logDir)) Directory.Delete(_logDir, true);
+        }
+    }
+}
